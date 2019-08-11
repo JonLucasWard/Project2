04d06d609fd0f8c7292ea851f3cb9d81d5184b07
unknown <galadionel@gmail.com>
2019-08-11 16:41:44 -0400

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Switch, Route } from "react-router-dom";
+import SignInSide from './components/Login_Component';
+import SignUp from './components/Signup_Component';
+import MainAppBar from './components/NavBar_Component';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+     
+        <Switch>
+        <MainAppBar/>
+        <Route path ="/" exact component={SignInSide}/>
+        <Route path ="/" exact component={SignUp}/>
+        </Switch>
+     
+    </HashRouter>
   );
 }
 
