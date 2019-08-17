bb16aeb63bcaf569e6d453b3d12941132d1c85fe
unknown <galadionel@gmail.com>
2019-08-17 19:22:45 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { HashRouter, Switch, Route } from "react-router-dom";
-import SignInSide from './components/Login_Component';
-import SignUp from './components/Signup_Component';
+import SignInSide from './components/LogIn_Main_Component';
+import SignUp from './components/Sign_Up_Main_Component';
 import MainAppBar from './components/NavBar_Component';
+import { Home } from './components/Home_Component';
+import Login_SignUp from './components/LogIn_SignUp_Component';
 
 const App: React.FC = () => {
   return (
     <HashRouter>
-     
+      <MainAppBar/>
         <Switch>
-        <MainAppBar/>
-        <Route path ="/" exact component={SignInSide}/>
-        <Route path ="/" exact component={SignUp}/>
+        
+        <Route path ="/" exact component={Home}/>
+        <Route path ="/Home" exact component={Home}/>
+        <Route path ="/Login" exact component={Login_SignUp}/>
         </Switch>
      
     </HashRouter>
--- src/components/NavBar_Component.tsx
+++ src/components/NavBar_Component.tsx
@@ -6,6 +6,15 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Link } from '@material-ui/core';
+import { getJwt } from '../helper/jwt';
+
+
+
+const ITEM_HEIGHT = 48;
+const jwt = getJwt();
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -23,18 +32,66 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function MainAppBar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  function handleClick(event: React.MouseEvent<HTMLElement>) {
+    setAnchorEl(event.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
+  function checkLogedIn(){
+    const jwt = getJwt();
+    if(!jwt) {
+        
+    }
+  }
+
+  function logout(){
+    localStorage.removeItem('cool-jwt');
+    window.location.reload();
+   
+}
 
   return (
-    <div className={classes.root}>
+    <div id="nav_bar_div" className={classes.root}>
+      
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <IconButton onClick={handleClick} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon>
+            </MenuIcon>
+              
           </IconButton>
+          <Menu
+                  id="long-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={open}
+                  onClose={handleClose}
+                  PaperProps={{
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                      width: 200,
+                    },
+                  }}
+                >
+          
+                    <MenuItem  onClick={handleClose}>
+                      <Button  href="/Home">Home</Button>
+                    </MenuItem>
+                
+              </Menu>
+              
+             
           <Typography variant="h6" className={classes.title}>
-           
+          
           </Typography>
-          <Button color="inherit">Login</Button>
+          {jwt ? <Button onClick={logout} href="#Home" color="inherit">Logout</Button>:<Button  href="#Login" color="inherit">Login</Button> } 
+          
         </Toolbar>
       </AppBar>
     </div>
